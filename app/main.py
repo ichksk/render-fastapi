3adb1a166c99439dda315068a23e609dbd52fe93
ichksk <good.ksuke.001@gmail.com>
2024-12-09 19:14:26 +0900

    db接続7

--- app/main.py
+++ app/main.py
@@ -10,13 +10,12 @@ from sqlmodel import create_engine, SQLModel
 from ws import updater
 
 engine = create_engine(os.environ["DATABASE_URL"], echo=True)
-print(os.environ["DATABASE_URL"])
-def create_db():
-    SQLModel.metadata.create_all(engine)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print("startup event")
+    print(os.environ["DATABASE_URL"])
+    print(engine)
+    SQLModel.metadata.create_all(engine)
     yield
     print("shutdown event")
 
